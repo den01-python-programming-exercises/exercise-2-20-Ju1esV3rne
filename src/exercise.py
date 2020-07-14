a867cd8639ad6296a3f2e79c1de677879925acba
Vladimir Slysar <g7au2ss@gmail.com>
2020-07-14 23:31:18 +0700

    1

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,26 @@
 def main():
-    #write your code below this line
+    number = 0
+    cur = 0
+    sum = 0
+    even = 0
+    odd = 0
+
+    while True:
+        number = int(input("Give numbers:"))
+        if number == -1:
+            break
+        elif number % 2 == 0:
+            even += 1
+        else:
+            odd += 1
+        sum += number
+        cur += 1
+        avg = sum/cur
+
+    print("Thx! Bye!\n" + "Sum: " + str(sum))
+    print("Numbers: " + str(cur))
+    print("Average: " + str(avg))
+    print("Even: " + str(even) + "\n" + "Odd: " + str(odd))
 
 if __name__ == '__main__':
     main()
